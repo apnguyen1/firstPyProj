764d403d754ec27a46d801709e51627a473d2203
Andrew <andrewnguyen17678@gmail.com>
2022-06-08 17:03:27 -0700

    V1

---
+++ RockPaperScissors.py
@@ -0,0 +1,39 @@
+import random
+
+states = ["Rock", "Paper", "Scissors"]
+
+def introduction():
+    print("Welcome to Andrew's First Python Program: Rock, Paper, Scissors!")
+    print("You'll be playing against an AI that'll choose one of the three choices.")
+    print()
+
+def get_user_input():
+    print("What would you like to play?")
+    user_input = input("Rock (R), Paper (S), or Scissors (S)?: ")
+
+    for state in states:
+        if(state[0].con)
+
+        if(state[0].lower() == user_input.lower()):
+            user_input = state
+
+    return user_input
+
+def confirm_user_input(user_input):
+    print(f"You chose {user_input}")
+    confirmation = input("To make sure this is the right choice, please type in Yes if correct, or No if wrong: ")
+
+def get_computer_choice():
+    
+
+
+
+# introduction()
+# user_input = get_user_input()
+# confirm_user_input(user_input)
+get_computer_choice()
+
+
+
+
+
